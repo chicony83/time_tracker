fe3fdb8ececb260c0c19297ccef14c9d0e57e2e9
chicony83 <chicony83@gmail.com>
2022-05-15 18:35:30 +0200

    UtilsCoroutines.kt added

---
+++ app/src/main/java/com/chico/time_tracker/utils/UtilsCoroutines.kt
@@ -0,0 +1,24 @@
+package com.chico.time_tracker.utils
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+
+fun launchIo(task: suspend () -> Unit) {
+    CoroutineScope(Dispatchers.IO).launch {
+        task()
+    }
+}
+
+fun launchUi(task: suspend () -> Unit) {
+    CoroutineScope(Dispatchers.Main).async {
+        task()
+    }
+}
+
+suspend fun <R> launchForResult(task: suspend () -> R): R? {
+    return CoroutineScope(Dispatchers.IO).async {
+        task()
+    }.await()
+}
